c106feb34a0e9d2c53df7bcad8203d655741304d
Salman-S7 <122789883+Salman-S7@users.noreply.github.com>
2025-06-29 16:56:58 +0530

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
               <img
                 src="https://raw.githubusercontent.com/kickiniteasy/bolt-hackathon-badge/main/src/public/bolt-badge/black_circle_360x360/black_circle_360x360.png"
                 alt="Bolt.new"
-                className="w-full h-full object-contain p-2 animate-[spin_8s_linear_infinite] group-hover:animate-[spin_1s_linear_infinite]"
+                className="w-full h-full object-contain animate-[spin_8s_linear_infinite] group-hover:animate-[spin_1s_linear_infinite]"
               />
             </a>
           </div>
