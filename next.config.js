b9ca0ebba812c98765d6c2cf40c736aad8571427
Salman-S7 <122789883+Salman-S7@users.noreply.github.com>
2025-06-29 09:47:08 +0530

    Fix Prisma Client import error

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma']
   },
+  // Ensure Prisma client is available during build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
